a714447d599bf91129496a9942a76b95b53583b7
Maria Ninos <marianinos@Marias-MBP.attlocal.net>
2019-10-10 16:19:15 -0700

    Set module path to reuse cmake modules in test

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,11 +2,9 @@ from conans import ConanFile, CMake, tools
 
 class TestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package"
-
-    def imports(self):
-        self.copy("*.cmake")
 
     def test(self):
         cmake = CMake(self)
+        modulesPath = self.deps_cpp_info["cmake-modules"].rootpath
+        cmake.definitions["CMAKE_MODULE_PATH"] = modulesPath
         cmake.configure()
